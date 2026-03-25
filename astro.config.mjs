6fc1b38795baf4d6fb9f7b9156d0a1ca6c7cb7db
davox1019-arch <davox1019@gmail.com>
2026-03-26 01:29:34 +1100

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://daveschimneysweeping.com.au'
+  site: 'https://daveschimneysweeping.com.au',
+  integrations: [sitemap()]
 });
